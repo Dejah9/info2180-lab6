281953f89af592aec6d7b46110b20d661b793854
dejah <khadejahhylton@yahoo.com>
2017-11-01 20:00:41 +0000

    Exercise 3

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function myFunction1(){
 	let ajax_request = new XMLHttpRequest();
 	ajax_request.onreadystatechange = function(){
 		if(ajax_request.readyState === 4 && ajax_request.status === 200){
-			alert(ajax_request.responseText);
+			document.getElementById("result").innerHTML="<h3>Result</h3>"+ajax_request.responseText;
 		}
 	};
-	ajax_request.open("GET","request.php?q=definition",true);
+	ajax_request.open("GET","request.php?q="+document.getElementById("value").value,true);
 	ajax_request.send();
 }
