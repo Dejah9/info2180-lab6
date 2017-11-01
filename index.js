800469f51817bcbe5b815ac294e0394eb7b96a45
dejah <khadejahhylton@yahoo.com>
2017-11-01 20:58:05 +0000

    Exercise 4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = function(){
 	document.getElementById("search").addEventListener("click",myFunction1);
+	document.getElementById("get_all").addEventListener("click",myFunction2);
 };
 function myFunction1(){
 	let ajax_request = new XMLHttpRequest();
@@ -9,5 +10,22 @@ function myFunction1(){
 		}
 	};
 	ajax_request.open("GET","request.php?q="+document.getElementById("value").value,true);
+	ajax_request.send();
+}
+function myFunction2(){
+	let ajax_request = new XMLHttpRequest();
+	ajax_request.onreadystatechange = function(){
+		if(ajax_request.readyState === 4 && ajax_request.status === 200){
+			let result = "<ol>";
+			let xml = ajax_request.responseXML;
+			let terms = xml.getElementsByTagName("definition");
+			for(let index = 0; index < terms.length; index++){
+				result+="<li><h3>"+terms[index].getAttribute("name")+"</h3><p>"+terms[index].childNodes[0].nodeValue+"</p><p> -"+terms[index].getAttribute("author")+"</p></li>";
+			}
+			result+="</ol>";
+			document.getElementById("result").innerHTML="<h3>Result</h3>"+result;
+		}
+	};
+	ajax_request.open("GET","request.php?q=&all=true",true);
 	ajax_request.send();
 }
